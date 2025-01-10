23c2b86a66bb66dded82900a4339a08acf11cdec
annu429 <anandparmar@gmail.com>
2025-01-10 20:53:21 +0530

    update for war

--- src/main/java/com/anand/aws_learn/AwsLearnApplication.java
+++ src/main/java/com/anand/aws_learn/AwsLearnApplication.java
@@ -2,12 +2,18 @@ package com.anand.aws_learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AwsLearnApplication {
+public class AwsLearnApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsLearnApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
 }
--- src/main/java/com/anand/aws_learn/controller/AwsDemoPipeline.java
+++ src/main/java/com/anand/aws_learn/controller/AwsDemoPipeline.java
@@ -1,15 +1,15 @@
 package com.anand.aws_learn.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/demo")
+@Controller
 public class AwsDemoPipeline {
 
-    @GetMapping
-    public String getMessage(){
-        return "Aws Demo pipeline Demo";
+    @RequestMapping("/")
+    public String homePage(){
+        return "home";
     }
 }
